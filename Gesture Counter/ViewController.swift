aa87fa8311daeca0a5e33c0ec3a37fdaf0ff21e9
Mirko Cukich <mirkocukich@gmail.com>
2019-08-04 19:37:08 -0500

    Initial Commit

--- Gesture Counter/ViewController.swift
+++ Gesture Counter/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mirko Cukich on 8/4/19.
 //  Copyright © 2019 Digital Mirko. All rights reserved.
 //
+//  Swift Gesture Counter - Demo 4 of 30
 
 import UIKit
 
@@ -12,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
