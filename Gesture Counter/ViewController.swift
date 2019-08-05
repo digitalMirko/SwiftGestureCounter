f47fea1910096f026277e990c6b91e494c4a3471
Mirko Cukich <mirkocukich@gmail.com>
2019-08-04 20:08:47 -0500

    Program Complete

--- Gesture Counter/ViewController.swift
+++ Gesture Counter/ViewController.swift
@@ -11,11 +11,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // counter label
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    // number formatter
+    let numberFormatter = NumberFormatter()
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // int value for numbers, no floats
+        numberFormatter.allowsFloats = false
+        
     }
-
-
+    
+    // Gesture - Tap Gesture Recognizer
+    @IBAction func tappedOnScreen(_ sender: UITapGestureRecognizer) {
+        // increments number by 1
+        self.incrementCounter()
+    }
+    
+    // Gesture - Long Press Gesture Recognizer
+    @IBAction func longPressedOnScreen(_ sender: UILongPressGestureRecognizer) {
+        // increments number by 1
+        self.incrementCounter()
+    }
+    
+    // Reset Counter Button
+    @IBAction func resetButton(_ sender: Any) {
+        // resets counter to 0
+        counterLabel.text = "\(0)"
+    }
+    
+    // increments counter
+    func incrementCounter() {
+        if let number = numberFormatter.number(from: counterLabel.text!) {
+            counterLabel.text = "\(number.intValue + 1)"
+        }
+    }
+    
 }
 
